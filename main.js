18046479e83c5f507f02530e394d4469ca032307
kk77777 <kaushikgattani@gmail.com>
2021-01-21 12:09:36 +0530

    adding styles

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const weather = require('./weather');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index');
